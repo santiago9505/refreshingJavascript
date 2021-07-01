8458c2802d4c5e9190b619c1a800870326abe8e0
Santigeek <santigeek@hotmail.com>
2021-07-01 08:52:04 -0500

    Making better the game and adding javascript

--- project/index.js
+++ project/index.js
@@ -8,12 +8,33 @@ const back = document.getElementById("back");
 const rockSelection = document.getElementById("rock");
 const paperSelection = document.getElementById("paper");
 const scissorsSelection = document.getElementById("scissors");
+const figureSelection = document.getElementById("select");
+const responseContainer = document.getElementById("response");
+const resultUser = document.getElementById("responseUser");
+const resultCpu = document.getElementById("responseCpu");
+const text = document.getElementById("dynamicText");
+const playAgain = document.getElementById("playAgain");
+const humanWinIcon = document.getElementById("humanWin");
+const humanLostIcon = document.getElementById("humanLost");
+const humanTieIcon = document.getElementById("humanTie");
+const cpUWinIcon = document.getElementById("cpuWin");
+const cpuLostIcon = document.getElementById("cpuLost");
+const cpuTieIcon = document.getElementById("cpuTie");
+const scorePerson = document.getElementById("scorePerson");
+const scoreCpu = document.getElementById("scoreMachine");
 const rock = 1;
 const paper = 2;
 const scissors = 3;
-let aleatoria = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+let aleatoria;
 let user;
 let machine;
+let result;
+let scoreMachine = 0;
+let scoreUser = 0;
+
+const generatingAleatoria = () => {
+  aleatoria = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+};
 
 const esconderInicio = () => {
   header.style.display = "none";
@@ -35,56 +56,79 @@ function backToMenu() {
 
 const rockSelected = () => {
   user = rock;
-  console.log("user: " + user);
-  paperSelection.style.display = "none";
-  scissorsSelection.style.display = "none";
-  if (aleatoria === 1) {
-    cpu = rock;
-    console.log("cpu: " + cpu);
-  } else if (aleatoria === 2) {
-    cpu = paper;
-    console.log("cpu: " + cpu);
-  } else {
-    cpu = scissors;
-    console.log("cpu: " + cpu);
-  }
+  figureSelection.style.display = "none";
+  responseContainer.style.display = "flex";
+  resultUser.src = "./assets/static/rock.png";
+  functAleatoria();
 };
 
 const paperSelected = () => {
   user = paper;
-  console.log("user: " + user);
-  rockSelection.style.display = "none";
-  scissorsSelection.style.display = "none";
-  if (aleatoria === 1) {
-    cpu = rock;
-    console.log("cpu: " + cpu);
-  } else if (aleatoria === 2) {
-    cpu = paper;
-    console.log("cpu: " + cpu);
-  } else {
-    cpu = scissors;
-    console.log("cpu: " + cpu);
-  }
+  figureSelection.style.display = "none";
+  responseContainer.style.display = "flex";
+  resultUser.src = "./assets/static/paper.png";
+  functAleatoria();
 };
 
 const scissorsSelected = () => {
   user = scissors;
-  console.log("user: " + user);
-  paperSelection.style.display = "none";
-  rockSelection.style.display = "none";
-  if (aleatoria === 1) {
-    cpu = rock;
-    console.log("cpu: " + cpu);
-  } else if (aleatoria === 2) {
-    cpu = paper;
-    console.log("cpu: " + cpu);
-  } else {
-    cpu = scissors;
-    console.log("cpu: " + cpu);
-  }
+  figureSelection.style.display = "none";
+  responseContainer.style.display = "flex";
+  resultUser.src = "./assets/static/scissors.png";
+  functAleatoria();
 };
 
-const showCpu = () => {};
+const functAleatoria = () => {
+  generatingAleatoria();
+  if (aleatoria === rock) {
+    machine = rock;
+    resultCpu.src = "./assets/static/rock.png";
+  } else if (aleatoria === paper) {
+    machine = paper;
+    resultCpu.src = "./assets/static/paper.png";
+  } else if (aleatoria === scissors) {
+    machine = scissors;
+    resultCpu.src = "./assets/static/scissors.png";
+  }
+  showResult();
+};
+
+const restartText = () => {
+  text.textContent = "Rock, paper, scissors...";
+};
+
+const showResult = () => {
+  if (user === machine) {
+    result = "We got a tie";
+  } else if (user === rock && machine === paper) {
+    result = "Machines won";
+    scoreMachine += 1;
+  } else if (user === rock && machine === scissors) {
+    result = "Humans won";
+    scoreUser += 1;
+  } else if (user === paper && machine === rock) {
+    result = "Humans won";
+    scoreUser += 1;
+  } else if (user === paper && machine === scissors) {
+    result = "Machines won";
+    scoreMachine += 1;
+  } else if (user === scissors && machine === paper) {
+    result = "Humans won";
+    scoreUser += 1;
+  } else if (user === scissors && machine === rock) {
+    result = "Machines won";
+    scoreMachine += 1;
+  }
+  scorePerson.textContent = scoreUser;
+  scoreCpu.textContent = scoreMachine;
+  text.textContent = result;
+};
+
+const volverAJugar = () => {
+  responseContainer.style.display = "none";
+  figureSelection.style.display = "flex";
+  text.textContent = "Rock, paper, scissors...";
+};
 
 back.addEventListener("click", volverAlMenu);
 playBtn.addEventListener("click", esconderInicio);
@@ -92,3 +136,4 @@ playH.addEventListener("click", esconderInicio);
 rockSelection.addEventListener("click", rockSelected);
 paperSelection.addEventListener("click", paperSelected);
 scissorsSelection.addEventListener("click", scissorsSelected);
+playAgain.addEventListener("click", volverAJugar);
